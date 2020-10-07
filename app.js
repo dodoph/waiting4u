a00b9de148b289d554ac0fd980bfa93fec910d5a
mandy <linxia@oregonstate.edu>
2020-10-07 15:54:26 -0700

    setup Express.js and React

---
+++ app.js
@@ -0,0 +1,25 @@
+const   express         = require('express'),
+        bodyParser      = require('body-parser'),
+        path            = require('path');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.send('<h1>Welcome!!</h1>');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/login.html'));
+});
+
+app.get('/logout', (req, res) => {
+    res.redirect('/');
+});
+
+var port = process.env.PORT || 3000
+app.listen(port, function(req, res){
+	console.log('Server Has Started at port ' + port + '!');	
+});
