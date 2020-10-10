e7199603ef3cd0e88a31a783df366ee2c592f404
mandy <linxia@oregonstate.edu>
2020-10-09 22:35:27 -0700

    Added user authentication component

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,31 @@
-import React, { Fragment } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
-import Alert from './components/layout/Alert';
+import React, { Fragment, useEffect } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import Alert from "./components/layout/Alert";
 // Redux
-import { Provider } from 'react-redux';
-import store from './store';
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from './utils/setAuthToken';
+import "./App.css";
 
-import './App.css';
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
-const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Fragment>
-        <Navbar />
+const App = () => {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
+  return (
+    <Provider store={store}>
+      <Router>
+        <Fragment>
+          <Navbar />
           <Route exact path="/" component={Landing} />
           <section className="container">
             <Alert />
@@ -24,9 +34,10 @@ const App = () => (
               <Route exact path="/login" component={Login} />
             </Switch>
           </section>
-      </Fragment>
-    </Router>
-  </Provider>
-);
+        </Fragment>
+      </Router>
+    </Provider>
+  );
+};
 
 export default App;
