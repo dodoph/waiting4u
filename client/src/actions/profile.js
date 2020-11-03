81845497313cbc1adb45bf711fe0bae82c595395
mandy <linxia@oregonstate.edu>
2020-11-03 15:12:41 -0800

    Delete pet profile

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { setAlert } from "./alert";
 import { URL_HOST } from "../constant";
 
-import { GET_PROFILE, PROFILE_ERROR, GET_PET_PROFILE, GET_ADMINS_PET_PROFILES, GET_ALL_PET_PROFILES } from "./types";
+import { GET_PROFILE, PROFILE_ERROR, GET_PET_PROFILE, DELETE_PET_PROFILE, GET_ADMINS_PET_PROFILES, GET_ALL_PET_PROFILES } from "./types";
 
 // Global config settings
 const postConfig = {
@@ -17,6 +17,26 @@ const getConfig = {
     "Access-Control-Allow-Origin": "*",
   },
 }
+
+// Get user profile
+export const getCurrentUserProfile = () => async (dispatch) => {
+  try {
+    const user_id = localStorage.getItem("token");
+    console.log(user_id);
+    if (user_id) {
+      const res = await axios.get(`${URL_HOST}/users/${user_id}`, getConfig);
+      dispatch({
+        type: GET_PROFILE,
+        payload: res.data,
+      });
+    }
+  } catch (err) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
 
 // Get current admin profile
 export const getCurrentAdminProfile = () => async (dispatch) => {
@@ -57,7 +77,7 @@ export const getAdminPetProfiles = () => async (dispatch) => {
   }
 };
 
-// Create pet profile
+// Create a pet profile
 export const createPetProfile = (formData, history) => async dispatch => {
   try {
     const admin_id = localStorage.getItem("token");
@@ -68,6 +88,25 @@ export const createPetProfile = (formData, history) => async dispatch => {
     });
     dispatch(setAlert("Pet Profile Created", "success"));
     history.push("/admindashboard");
+  } catch(err) {
+    dispatch(setAlert(err.response.data.Error, "danger"));
+  }
+}
+
+// Delete a pet profile
+export const deletePetProfile = (pet_id) => async dispatch => {
+  try {
+    const admin_id = localStorage.getItem("token");
+    await axios.delete(`${URL_HOST}/admins/${admin_id}/pets/${pet_id}`);
+    console.log("deleted a pet profile");
+    // update reducer
+    const res = await axios.get(`${URL_HOST}/admins/${admin_id}/pets`, getConfig);
+      console.log(res);
+      dispatch({
+        type: GET_ADMINS_PET_PROFILES,
+        payload: res.data,
+      });
+    dispatch(setAlert("Pet Profile Deleted", "success"));
   } catch(err) {
     dispatch(setAlert(err.response.data.Error, "danger"));
   }
@@ -91,28 +130,5 @@ export const getAllPetProfiles = () => async (dispatch) => {
 };
 
 
-// Get user profile
-export const getCurrentUserProfile = () => async (dispatch) => {
-  try {
-    const config = {
-      headers: {
-        "Access-Control-Allow-Origin": "*",
-      },
-    };
-    const user_id = localStorage.getItem("token");
-    console.log(user_id);
-    if (user_id) {
-      const res = await axios.get(`${URL_HOST}/users/${user_id}`, config);
-      dispatch({
-        type: GET_PROFILE,
-        payload: res.data,
-      });
-    }
-  } catch (err) {
-    dispatch({
-      type: PROFILE_ERROR,
-      payload: { msg: err.response.statusText, status: err.response.status },
-    });
-  }
-};
+
 
--- client/src/components/dashboard/ManagePets.js
+++ client/src/components/dashboard/ManagePets.js
@@ -1,18 +1,42 @@
-import React, { useEffect, Fragment } from "react";
+import React, { useEffect, Fragment, useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import Spinner from "../layout/Spinner";
-import { getAdminPetProfiles } from "../../actions/profile";
-import { Table, Container } from "react-bootstrap";
+import { getAdminPetProfiles, deletePetProfile } from "../../actions/profile";
+import { Table, Container, Button } from "react-bootstrap";
 import PetList from "./PetList";
 
 const ManagePets = ({
   getAdminPetProfiles,
+  deletePetProfile,
   petProfile: { adminsPetProfiles, loading },
 }) => {
   useEffect(() => {
     getAdminPetProfiles();
   }, []);
+
+  // const [petData, setPetData] = useState(adminsPetProfiles);
+
+  // function handleRemove(pet_id) {
+  //   adminsPetProfiles = adminsPetProfiles.filter((pet) => pet.pet_id != pet_id);
+  //   this.setState();
+  //   deletePetProfile(pet_id);
+
+  //   console.log(`delete, ${pet_id}`);
+  // }
+
+  const pets = adminsPetProfiles ? (adminsPetProfiles.map((pet) => (
+    <tr key={pet.pet_id}>
+      <td>{pet.pet_name}</td>
+      <td>{pet.description}</td>
+      <td>
+        <Button>Edit</Button>
+      </td>
+      <td>
+        <Button onClick={() => deletePetProfile(pet.pet_id)}>Delete</Button>
+      </td>
+    </tr>
+  ))) : null;
 
   return loading && adminsPetProfiles === null ? (
     <Spinner />
@@ -34,9 +58,21 @@ const ManagePets = ({
             </tr>
           </thead>
           <tbody>
-            {adminsPetProfiles.map((pet, index) => {
-              return <PetList key={index} {...pet} />;
-            })}
+            {/* {adminsPetProfiles.map((pet) => (
+              <tr key={pet.pet_id}>
+                <td>{pet.pet_name}</td>
+                <td>{pet.description}</td>
+                <td>
+                  <Button>Edit</Button>
+                </td>
+                <td>
+                  <Button onClick={() => handleRemove(pet.pet_id)}>
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            ))} */}
+            {pets}
           </tbody>
         </Table>
       </Container>
@@ -46,6 +82,7 @@ const ManagePets = ({
 
 ManagePets.propTypes = {
   getAdminPetProfiles: PropTypes.func.isRequired,
+  deletePetProfile: PropTypes.func.isRequired,
   petProfile: PropTypes.object.isRequired,
 };
 
@@ -53,4 +90,7 @@ const mapStateToProps = (state) => ({
   petProfile: state.petProfile,
 });
 
-export default connect(mapStateToProps, { getAdminPetProfiles })(ManagePets);
+export default connect(mapStateToProps, {
+  getAdminPetProfiles,
+  deletePetProfile,
+})(ManagePets);
