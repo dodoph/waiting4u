65d8af8ecf65184ce38a1da2226307076902f476
Jessie Tang <tangqiaojie@hotmail.com>
2020-11-20 13:54:29 -0800

    rename getRecentStatusUpdate()

--- client/src/components/landing/MyCarousel.js
+++ client/src/components/landing/MyCarousel.js
@@ -2,21 +2,21 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import React, { useEffect } from "react";
 import { Carousel } from "react-bootstrap";
-import { getRecentUpdates } from "../../actions/profile";
+import { getRecentStatusUpdate } from "../../actions/profile";
 import Spinner from "../layout/Spinner";
 
 const MyCarousel = ({
-    getRecentUpdates,
+    getRecentStatusUpdate,
     petProfile: { petUpdateProfiles },
 }) => {
     useEffect(() => {
         if(!petUpdateProfiles) {
-            getRecentUpdates();
+            getRecentStatusUpdate();
         }
     }, [petUpdateProfiles]);
   return (
     <Carousel>
-        {petUpdateProfiles ? (petUpdateProfiles.map((pet, index) =>{
+        {petUpdateProfiles ? (petUpdateProfiles.map((pet) =>{
             return (
                 <Carousel.Item>
                     <img className="myCarousel-item" src={pet.image_url} alt="slide" />
@@ -32,7 +32,7 @@ const MyCarousel = ({
 };
 
 MyCarousel.propTypes = {
-    getRecentUpdates: PropTypes.func.isRequired,
+    getRecentStatusUpdate: PropTypes.func.isRequired,
     petProfile: PropTypes.object.isRequired,
 }
 
@@ -40,4 +40,4 @@ const mapStateToProps = (state) => ({
     petProfile: state.petProfile,
 });
 
-export default connect(mapStateToProps, { getRecentUpdates })(MyCarousel);
+export default connect(mapStateToProps, { getRecentStatusUpdate })(MyCarousel);
